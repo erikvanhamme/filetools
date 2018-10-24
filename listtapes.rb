e5d158490f0bc5374c56980b74a30959805937e4
Erik Van Hamme <erik.vanhamme@gmail.com>
2018-10-24 20:24:04 +0200

    Added list tapes tool.

---
+++ listtapes.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require_relative 'filetools'
+
+$state = State.new('list tapes tool')
+
+begin
+	init
+    db = $state.db
+
+    unless $state.quiet
+        tapes = db.execute('SELECT * FROM tapes')
+        puts "Number:\tLabel:\t\tTape size:\tTapeset:\tTapeset idx:"
+        tapes.each do |tape|
+            puts "#{tape[0]}\t#{tape[1]}\t#{tape[2]}\t#{tape[3]}\t\t#{tape[4]}"
+        end
+    end
+rescue SQLite3::Exception => e 
+    puts "Database exception occurred:"
+    puts e
+ensure
+    db = $state.db
+    db.close if db
+end
